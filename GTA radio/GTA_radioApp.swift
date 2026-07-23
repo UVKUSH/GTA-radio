c54de631ae77ad637c8e505764c8200440f0dabc
UVKUSH <uv2647@icloud.com>
2026-07-23 16:26:34 -0400

    Initial Commit

---
+++ GTA radio/GTA_radioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTA_radioApp.swift
+//  GTA radio
+//
+//  Created by UV Kush on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct GTA_radioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
